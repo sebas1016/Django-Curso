4a27f2d225b40a11c1f7203e26964ae2cbba58b5
sebas1016 <sebas16berrio@gmail.com>
2024-02-21 18:04:43 -0500

    Se agregan Tags y filtros

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,18 +6,24 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
 # Create your views here.
 #Views por clases
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+#class PostListView(ListView):
+#    queryset = Post.published.all()
+#    context_object_name = 'posts'
+#    paginate_by = 3
+#    template_name = 'blog/post/list.html'
 
 #Views por Funciones
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     paginator = Paginator(post_list,3)
     page_number = request.GET.get('page', 1)
     try:
@@ -28,7 +34,8 @@ def post_list(request):
         posts = paginator.page(1)
     return render(request,
                   'blog/post/list.html',
-                  {'posts':posts})
+                  {'posts':posts,
+                   'tag':tag})
   
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
@@ -39,11 +46,18 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
     comments = post.comments.filter(active=True)
     form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                          .exclude(id=post.id)
+    
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                            .order_by('-same_tags','-publish')[:4]                                          
     return render(request,
                   'blog/post/detail.html',
                   {'post':post,
                    'comments':comments,
-                   'form':form})
+                   'form':form,
+                   'similar_posts':similar_posts})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PULISHED)
